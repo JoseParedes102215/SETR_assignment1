87045347916993dac0e4b572f504995e2f8a8bd9
JoseParedes <jose.paredes@ua.pt>
2023-03-07 19:03:13 +0000

    print que printa valores da fifo e teste na main

--- fifo.c
+++ fifo.c
@@ -18,26 +18,6 @@ struct FIFO{
     int tail; // posição do próximo elemento a ser inserido
 };
 
-<<<<<<< HEAD
-int isFifoFull(*FIFO fifo){
-    return ((fifo>tail - 1 ) ==  MAX_SIZE);
-}
-
-int isFifoEmpty(){
-    return (fifo->tail == fifo->head);
-}
-
-
-void MyFIFOInsert(*FIFO fifo,int value){
-    if(isFifoFull(fifo) == 1){
-        return -1;
-    }
-    fifo.buffer[tail] = value;
-    fifo.tail +=  1;
-    return 0;
-}
-
-
 
 // inicializa o FIFO
 void MyFIFOInit(struct FIFO *fifo){
@@ -46,21 +26,57 @@ void MyFIFOInit(struct FIFO *fifo){
 }
 
 
+
+int isFifoFull(struct FIFO *fifo){
+    return ((fifo->tail) ==  MAX_SIZE);
+     
+}
+
+int isFifoEmpty(struct FIFO *fifo){
+    return (fifo->tail == fifo->head);
+}
+
+
+int MyFIFOInsert(struct FIFO *fifo, int value){
+    if(isFifoFull(fifo) == 1){
+        return -1;
+    }
+    fifo->buffer[fifo->tail] = value;
+    fifo->tail =   fifo->tail + 1;
+    return 0;
+}
+
+void PrintFifo(struct FIFO *fifo){
+    for (int i = 0; i < MAX_SIZE; i++)
+    {
+        printf("%d\n",fifo->buffer[i]);
+    }
+    
+}
+
 int main(){
-/*
+
 struct FIFO fifo1;
 MyFIFOInit(&fifo1);
-    for (int i = 0; i < MAX_SIZE + 1 ;i++)
+for (int i = 0; i < MAX_SIZE + 1 ;i++)
     {
-        if(MyFIFOInsert(fifo1,2*i) == 0);
+        if(MyFIFOInsert(&fifo1,2*i) == 0){
+            printf("Adicionou o elemento com sucesso \n");
+        }
 
+        else{
+            printf("fifo cheio \n");
+        }
     }
-   
+
+    PrintFifo(&fifo1);
+
+   /*
     fifo1.buffer[0] = 11;
     printf("O primeiro elemento e %d",fifo1.buffer[0]);
     printf("\no head é %d",fifo1.head);
-    return 0;
-
+    
 */
+return 0;
     
 }
